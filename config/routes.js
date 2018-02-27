64b39f2d86dd811931b5ad990839e73971c91b81
Mark S <markscicluna@hotmail.com>
2018-02-27 13:46:29 +0000

    created routes for all 4 models

--- config/routes.js
+++ config/routes.js
@@ -1,6 +1,37 @@
 const router = require('express').Router();
 
+const walks = require('../controllers/walks');
+const dogs = require('../controllers/dogs');
+const users = require('../controllers/users');
+const exercise = require('../controllers/exercise');
+
 // routes go here
+router.route('/walks')
+  .get(walks.index)
+  .post(walks.create);
+
+router.route('/walks/:id')
+  .get(walks.show)
+  .put(walks.update)
+  .delete(walks.delete);
+
+router.route('/dogs')
+  .post(dogs.create);
+
+router.route('/dogs/:id')
+  .get(dogs.show)
+  .put(dogs.update)
+  .delete(dogs.delete);
+
+router.route('/users')
+  .get(users.index);
+
+router.route('/users/:id')
+  .get(users.show)
+  .put(users.update);
+
+router.route('/dogs/:id/exercise')
+  .get(exercise.show);
 
 router.all('/*', (req, res) => res.notFound());
 
