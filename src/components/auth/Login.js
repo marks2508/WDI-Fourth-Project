0da7cae3142b1c3071cd863ef4aacd765be3b997
Mark S <markscicluna@hotmail.com>
2018-02-27 15:17:49 +0000

    added login component

---
+++ src/components/auth/Login.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import Axios from 'axios';
+import LoginForm from './LoginForm';
+import Auth from '../../lib/Auth';
+
+class Login extends React.Component {
+  state = {
+    user: {
+      email: '',
+      password: ''
+    },
+    errors: {}
+  };
+
+  handleChange = ({target: {name, value}}) => {
+    const user = Object.assign({}, this.state.user, {[name]: value});
+    this.setState({user});
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    Axios
+      .post('/api/login', this.state.user)
+      .then(res => {
+        Auth.setToken(res.data.token);
+        this.props.history.push('/');
+      })
+      .catch(err => this.setState({errors: err.response.data.errors}));
+  }
+  render() {
+    return (
+      <LoginForm
+        user={this.state.user}
+        handleChange={this.handleChange}
+        handleSubmit={this.handleSubmit}
+        errors={this.state.errors}
+      />
+    );
+  }
+}
+
+export default Login;
