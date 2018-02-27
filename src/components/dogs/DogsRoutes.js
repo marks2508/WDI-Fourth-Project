a708b6c9945726337de47d7b36fe5890e7588eeb
Mark S <markscicluna@hotmail.com>
2018-02-27 16:08:24 +0000

    done dogsRoutes component

---
+++ src/components/dogs/DogsRoutes.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {Switch, Route} from 'react-router-dom';
+
+import DogsShow from './DogsShow';
+import DogsNew from './DogsNew';
+import DogsEdit from './DogsEdit';
+
+const DogsRoutes = () => {
+  return (
+    <Switch>
+      <Route path="/dogs/new" component={DogsNew} />
+      <Route path="/dogs/:id/edit" component={DogsEdit} />
+      <Route path="/dogs/:id" component={DogsShow} />
+    </Switch>
+  );
+};
+
+export default DogsRoutes;
